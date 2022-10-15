0ad22fe224fa3904775d869efa3e005b95c506f4
Damian Barabonkov <damianb@mit.edu>
2022-10-15 23:22:46 +0200

    Integrate tests using 'TxnElemsContext'

--- tests/test_recurring_payments.py
+++ tests/test_recurring_payments.py
@@ -4,11 +4,9 @@ import algosdk
 from algopytest import (
     payment_transaction,
     smart_signature_transaction,    
-    txn_elem,
-    with_txn_id,
+    TxnElemsContext,
     group_transaction,
     suggested_params,
-    transaction_info,
 )
 
 TMPL_FEE = 1000
@@ -25,32 +23,34 @@ def test_pull_payment_single_receiver(smart_signature, owner, user1):
     params = suggested_params(flat_fee=True, fee=TMPL_FEE)
     params.first = round_down(params.first, TMPL_PERIOD)
     params.last = params.first + TMPL_DURATION
-    
-    _, txn = txn_elem(payment_transaction)(sender=owner, receiver=user1, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_0', params=params)
-    txn_id, _ = with_txn_id(smart_signature_transaction)(smart_signature, txn)
 
-    assert 'logicsig' in transaction_info(txn_id)['transaction']['signature']
+    with TxnElemsContext():
+        _, txn = payment_transaction(sender=owner, receiver=user1, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_0', params=params)
+
+    smart_signature_transaction(smart_signature, txn)
 
 def test_pull_payment_multiple_receiver(smart_signature_two_receivers, owner, user1, user2):
     params = suggested_params(flat_fee=True, fee=TMPL_FEE)
     params.first = round_down(params.first, TMPL_PERIOD)
     params.last = params.first + TMPL_DURATION
-    
-    _, txn0 = txn_elem(payment_transaction)(sender=owner, receiver=user1, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_0', params=params)
-    _, txn1 = txn_elem(payment_transaction)(sender=owner, receiver=user2, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_1', params=params)
 
-    group_transaction(
-        txn_elem(smart_signature_transaction)(smart_signature_two_receivers, txn0),
-        txn_elem(smart_signature_transaction)(smart_signature_two_receivers, txn1),
-    )
+    with TxnElemsContext():
+        _, txn0 = payment_transaction(sender=owner, receiver=user1, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_0', params=params)
+        _, txn1 = payment_transaction(sender=owner, receiver=user2, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_1', params=params)
+
+        lsig_txn0 = smart_signature_transaction(smart_signature_two_receivers, txn0)
+        lsig_txn1 = smart_signature_transaction(smart_signature_two_receivers, txn1)
+
+    group_transaction(lsig_txn0, lsig_txn1)
 
 def test_pull_payment_raises_invalid_first_round(smart_signature, owner, user1):
     params = suggested_params(flat_fee=True, fee=TMPL_FEE)
     # The first valid round is no longer a multiple of `TMPL_PERIOD` which will cause the failure
     params.first = round_down(params.first, TMPL_PERIOD) + 1
     params.last = params.first + TMPL_DURATION
-    
-    _, txn = txn_elem(payment_transaction)(sender=owner, receiver=user1, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_0', params=params)
+
+    with TxnElemsContext():
+        _, txn = payment_transaction(sender=owner, receiver=user1, amount=TMPL_AMOUNT, lease=f'{TMPL_LEASE}_0', params=params)
 
     with pytest.raises(algosdk.error.AlgodHTTPError, match=r'transaction .* invalid : transaction .* rejected by logic'):
         smart_signature_transaction(smart_signature, txn)
